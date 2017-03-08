060ad853586a1c38540654847158ccf59d1c8e00
fakestarbaby <fakestarbaby@gmail.com>
2017-03-09 00:07:58 +0900

    Implement search nippo emoji reaction

--- inug.rb
+++ inug.rb
@@ -18,5 +18,8 @@ messages["messages"]["matches"].each do |message|
   reactions = Slack.client.reactions_get(channel: target_channel["id"], timestamp: message["ts"])
   next if reactions["message"]["reactions"].nil?
 
-  pp reactions["message"]["reactions"]
+  if (reactions["message"]["reactions"].select {|reaction| reaction["name"] == ENV["SLACK_NIPPO_EMOJI_REACTION"]}.size != 0)
+    pp message["text"]
+    break
+  end
 end
